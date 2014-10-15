922600004fc59a0501979823d03a42275ee7dde0
Jacob Clark <jacob.jh.clark@googlemail.com>
2014-10-15 15:13:50 -0400

    Constructor to prepare numerals to numbers

--- RomanNumeralGeneratorVanilla/src/RomanNumeralGenerator.php
+++ RomanNumeralGeneratorVanilla/src/RomanNumeralGenerator.php
@@ -2,6 +2,13 @@
 
 class RNGenerator implements RomanNumeralGenerator{
 
+	function __construct() {
+                // Numerals
+                $this->numerals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+                // Decimals
+                $this->numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+        }
+
         public function generate($integer){
 
 		if($integer < 1 || $integer > 3999) 
