543722b0a5a013fc79fce03b10f6f2700e77ff37
Jacob Clark <jacob.jh.clark@googlemail.com>
2014-10-15 14:58:08 -0400

    Implimenting interface methods

---
+++ RomanNumeralGeneratorVanilla/src/RomanNumeralGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+require('RomanNumeralGeneratorInterface.php');
+
+class RNGenerate implements RomanNumeralGenerator{
+
+        public function generate($integer){
+        }
+
+        public function parse($string){
+        }
+}
