6255b888c339a57ae01752c1e94a37c4b28ae7eb
Jacob Clark <jacob.jh.clark@googlemail.com>
2014-10-16 22:26:58 +0100

    Fixing PHP memory leak

--- PHP/RomanNumeralGeneratorSlimAPI/index.php
+++ PHP/RomanNumeralGeneratorSlimAPI/index.php
@@ -4,20 +4,20 @@ require 'vendor/autoload.php';
 require('../autoload.php');
 $app = new \Slim\Slim();
 
-$app->get('/getInteger/:int', 'getInteger');
-$app->get('/getNumerals/:string', 'getNumerals'); 
+$app->get('/getInteger/:string', 'getInteger');
+$app->get('/getNumerals/:int', 'getNumerals'); 
 
 $app->run();
-
-function getNumerals($int){
-	$rng = new RNGenerator();
-	$result = $rng->generate($int);
-	echo '{"result":"'.$result.'"}';
-}
-
 
 function getInteger($string){
 	$rng = new RNGenerator();
 	$result = $rng->parse($string);
+	echo '{"result":"'.$result.'"}';
+}
+
+
+function getNumerals($int){
+	$rng = new RNGenerator();
+	$result = $rng->generate($int);
 	echo '{"result":"'.$result.'"}';
 }
