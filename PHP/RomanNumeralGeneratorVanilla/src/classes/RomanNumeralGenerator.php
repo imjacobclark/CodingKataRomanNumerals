a35b7fe07b8096fc8c56172626991df326b5ecf7
Jacob Clark <jacob.jh.clark@googlemail.com>
2014-10-16 15:33:25 +0100

    AssosArray PHP

--- PHP/RomanNumeralGeneratorVanilla/src/classes/RomanNumeralGenerator.php
+++ PHP/RomanNumeralGeneratorVanilla/src/classes/RomanNumeralGenerator.php
@@ -2,14 +2,20 @@
 class RNGenerator implements RomanNumeralGenerator{
 
 	function __construct() {
-		/*// Numerals
-		$this->numerals = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
-		// Decimals
-		$this->numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-		*/
-
-		// Numerals
-		$this->numerals = ["M" => 1000, "CM" => 900, "D" => 500, "CD" => 400, "C" => 100, "XC" => 90, "L" => 50, "XL" => 40, "X" => 10, "IX" => 9, "V" => 5, "IV" => 4, "I" => 1];
+		$this->numerals = [	"M" 	=> 1000, 
+							"CM" 	=> 900, 
+							"D" 	=> 500, 
+							"CD" 	=> 400, 
+							"C" 	=> 100, 
+							"XC" 	=> 90, 
+							"L" 	=> 50, 
+							"XL" 	=> 40, 
+							"X" 	=> 10, 
+							"IX" 	=> 9, 
+							"V" 	=> 5, 
+							"IV" 	=> 4, 
+							"I" 	=> 1
+							];
 	}
 
 	public function generate($integer){
